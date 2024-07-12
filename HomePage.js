7b8ad5359635c66f6d6de442c661806921d91087
ilyass <ilyassovitche1234@gmail.com>
2024-07-12 13:06:45 +0100

    add (functions/style/elements)

--- HomePage.js
+++ HomePage.js
@@ -35,9 +35,55 @@ function navigation()
 navigation();
 
 //===================================================================
+    // in progress + done
+// function inprogress()
+// {
+//     let exist=document.getElementById('pending');
+//   if(!exist)
+//     {
+//       let mess=document.createElement('input');
+//       mess.type='te';
+//       mess.placeholder='Enter progress details...';
+//       mess.id='pending';
+//       let progress =document.getElementById("Progresslabel");
+//       progress.insertAdjacentElement('afterend',mess);
+//     }
+//   else
+//     {
+//       document.getElementById('progressRadio').checked = false;
+//       exist.remove();
+//     }
+  
+// }
+function inprogress()
+{
+    let exist=document.getElementById('pending');
+  if(!exist)
+    {
+      let mess=document.createElement('textarea');
+      mess.rows='4';
+      mess.placeholder='Enter progress details...';
+      mess.id='pending';
+      let progress =document.getElementById("Progresslabel");
+      progress.insertAdjacentElement('afterend',mess);
+    }
+  else
+    {
+      document.getElementById('progressRadio').checked = false;
+      exist.remove();
+    }
+  
+}
 
 
+function done()
+{
+  let exist=document.getElementById('pending');
+  if(exist)
+  exist.remove();
+}
 
+//=========================================================================
 
 
 
